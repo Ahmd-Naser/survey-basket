cd1a93e13450846c1996692af75ac4797f8bc2b2
ahmd-naser <ahmdnasr197@gmail.com>
2026-04-30 05:33:33 +0300

    IsDisabled Column added to ApplicationUser table

--- SurveyBasket.Api/Errors/UserErrors.cs
+++ SurveyBasket.Api/Errors/UserErrors.cs
@@ -5,6 +5,10 @@ public static class UserErrors
     public static readonly Error InvalidCredentials =
         new( "User.InvalidCredentials","Invalid username Or password" , StatusCodes.Status401Unauthorized );
 
+    public static readonly Error DisabledUser =
+       new("User.DisabledUser", "Disabled User , please contact your administartor", StatusCodes.Status401Unauthorized);
+
+
     public static readonly Error InvalidJwtToken =
         new("User.InvalidJwtToken", "Invalid Token", StatusCodes.Status401Unauthorized);
 
---
+++ SurveyBasket.Api/Persistence/Migrations/20260430022141_AddIsDisabledColumnToUsers.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyBasket.Api.Presistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDisabledColumnToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDisabled",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "019d914e-f324-7a2c-9459-d1c6af7d44e9",
+                columns: new[] { "IsDisabled", "PasswordHash" },
+                values: new object[] { false, "AQAAAAIAAYagAAAAEEnJIU3S8VQCHHYLJ8y0bTC+fLKqpWW2peCurfCTn92eJldRD8DoJWoUlyrrT7VOKA==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDisabled",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "019d914e-f324-7a2c-9459-d1c6af7d44e9",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEEb/gEH0giDmiEYTrQQEEqsH4IvYvJLFNCwVn+zhyB1JlJq5OWph6z76B6B93tzQkw==");
+        }
+    }
+}
